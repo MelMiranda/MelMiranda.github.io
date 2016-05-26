52a1b3a75fa544f3c41e642b79992a74b2e87d51
Mel Miranda <melania.smc@hotmail.com>
2016-05-25 22:46:46 -0300

    22:45 25-05-2016

---
+++ js.js
@@ -0,0 +1,47 @@
+
+
+$(document).on("ready",function(){
+    slider();
+} );
+
+
+function slider(){
+
+var currentIndex = 0,
+  items = $('.description-section div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.description-section div').eq(currentIndex);
+  items.hide();
+  item.fadeIn("slow");
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
+
+ console.log("checkpoint-2");
+}
