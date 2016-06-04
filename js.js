dceaf4274f7291c96907a56dc5d13a033acdcb04
Mel Miranda <melania.smc@hotmail.com>
2016-06-04 18:52:18 -0300

    18:52 4-06-2016

--- js.js
+++ js.js
@@ -3,6 +3,7 @@
 $(window).on("load",function(){
 
  loading();
+  SameHeight();
 
 });
 
@@ -205,3 +206,36 @@ function ajustarAlto(){
     };
 
 };
+
+
+function ResetHeight(){
+    var id=1;
+    while($( "[hid="+id+"]" ).length||id<20){
+      
+      $("[hid="+id+"]").css("height","auto");
+      id++;
+    }
+  }
+  
+  var headerHeight=0;
+  var distanceHeader=0;
+  SetDistanceHeader();
+  
+  function SameHeight(){
+    //console.log("entra same height");
+    
+    var auxId=1;
+    while($( "[hid="+auxId+"]" ).length||auxId<20){
+      var hidHeight=0;
+      //ResetHeight();
+      $("[hid="+auxId+"]").each(function(){
+        if($(this).innerHeight()>hidHeight){
+          hidHeight= $(this).innerHeight();
+        }
+        
+      });
+      //console.log(hidHeight);
+      $("[hid="+auxId+"]").css("height",hidHeight+"px");
+      auxId++;
+    }
+  }
